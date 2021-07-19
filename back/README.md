e232db6d4acef4ef67be9ec178770493004f523b
Kevin Andries <kevin.andries@yahoo.fr>
2021-07-19 19:43:28 +0200

    Initial commit

---
+++ back/README.md
@@ -0,0 +1,7 @@
+To start the app, run in a terminal :
+
+`npm run watch`
+
+And in another terminal, run :
+
+`npm start`
---
+++ back/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({
+	path: `${__dirname}/../config.env`,
+});
+const app = express();
+const PORT = process.env.PORT as string;
+
+app.get("/", (_, res) => {
+	res.status(200).json({
+		message: "Hello World",
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
