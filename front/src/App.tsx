86e553ab6106322ab3c891de3ec11918a135d871
JonHualde <jonhualde94@gmail.com>
2021-07-19 21:45:13 +0100

    First commit
    Prettier config file added

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-	return <div></div>;
+  return (
+    <div>
+      <div style={{ height: "15vh" }}>Visio-app</div>
+      <div style={{ height: "70vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <button style={{ marginBottom: "10px" }}>Create a room</button>
+        <button>Join a room</button>
+      </div>
+      <div style={{ height: "15vh" }}>All rights reserved</div>
+    </div>
+  );
 }
 
 export default App;
