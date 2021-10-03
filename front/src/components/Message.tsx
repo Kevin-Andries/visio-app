d5becd66a344ee1185611312acc4de2c4dced60d
Kevin Andries <kevin.andries@yahoo.fr>
2021-10-03 08:46:06 +0200

    Fix missing 0 when displaying minutes

--- front/src/components/Message.tsx
+++ front/src/components/Message.tsx
@@ -5,14 +5,22 @@ interface IProps {
   showAuthor: boolean;
 }
 
-const Message = ({ msg: { author, msgText }, showAuthor }: IProps) => {
+function formatTime() {
   const date = new Date();
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+
+  return `${hours}h${minutes}`;
+}
+
+const Message = ({ msg: { author, msgText }, showAuthor }: IProps) => {
   return (
     <div className={showAuthor ? "mt-3" : ""}>
       {showAuthor && (
         <p className="font-bold flex items-center justify-between pr-2">
           <span>{author}</span>
-          <span className="text-xs text-gray-400 font-normal">{`${date.getHours()}h${date.getMinutes()}`}</span>
+          <span className="text-xs text-gray-400 font-normal">{formatTime()}</span>
         </p>
       )}
       <p style={{ wordWrap: "break-word", paddingRight: "5px" }}>{msgText}</p>
